bf5985ec434085ebe4a199097ec0f5a0cb75d46c
lucarl07 <junitinho2007@gmail.com>
2026-01-20 11:36:36 -0300

    feat: Write the basic structure with placeholder data ✨

---
+++ src/components/Post.vue
@@ -0,0 +1,14 @@
+<script setup>
+const { title, body } = defineProps(['title', 'body'])
+</script>
+
+<template lang="html">
+  <article class="post">
+    <h3 class="title">{{ title }}</h3>
+    <p class="body">{{ body }}</p>
+  </article>
+</template>
+
+<style lang="css" scoped>
+  
+</style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Post from 'components/Post.vue';
+
+const { posts } = defineProps(['posts'])
+</script>
+
+<template lang="html">
+  <section class="feed">
+    <h2>Postagens para você</h2>
+    <Post v-for="post in posts" :key="post.id" 
+      :title="post.title" :body="post.body" />
+  </section>
+</template>
+
+<style lang="css" scoped>
+  
+</style>
