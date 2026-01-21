1f8ae8277243ba97f1a1aaaf8cc2769e8d78e04c
lucarl07 <junitinho2007@gmail.com>
2026-01-21 15:28:52 -0300

    Stylize App & PostList components 💄

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,50 @@ loadPosts()
 </script>
 
 <template>
-  <h1>Página Inicial</h1>
-  <PostList v-if="posts.length" :posts="posts">
-    <template v-slot:header>
-      <button @click="loadPosts">Recarregar dados</button>
-    </template>
-  </PostList>
-  <Error v-else-if="error" title="Um erro ocorreu no sistema!" :message="error" />
-  <LoadingScreen v-else message="Carregando postagens..." />
+  <main>
+    <h1>Página Inicial</h1>
+    <PostList v-if="posts.length" :posts="posts">
+      <template v-slot:header>
+        <button class="reload-button" @click="loadPosts">
+          Recarregar dados
+        </button>
+      </template>
+    </PostList>
+    <Error v-else-if="error" title="Um erro ocorreu no sistema!" :message="error" />
+    <LoadingScreen v-else message="Carregando postagens..." />
+  </main>
 </template>
 
+<style> /* Global styles */
+  body {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    background-color: var(--dark-gray-1);
+    color: white;
+  }
+</style>
+
 <style scoped>
+  main {
+    display: flex;
+    flex-direction: column;
+    padding: 2rem;
+    min-width: 50%;
+    background-color: var(--dark-gray-2);
+    color: white;
+  }
+
+  h1 {
+    margin-bottom: 2rem;
+  }
+
+  .reload-button {
+    border-radius: .75rem;
+  }
+  .reload-button:hover {
+    text-decoration: underline;
+  }
 </style>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -14,25 +14,33 @@ const filteredPosts = filterPosts(posts, filter, true)
 <template lang="html">
   <section class="feed">
     <header>
-      <h2 class="title">Postagens para você</h2>
-      <input 
-        type="text" id="filter-by" v-model="filter"
-        placeholder="Buscar por título..." />
-      <slot name="header"></slot>
+      <h2 class="title">Postagens para você:</h2>
+      <div class="search-wrapper">
+        <input type="text" id="search-by"
+          placeholder="Buscar por título..."
+          v-model="filter" />
+        <slot name="header"></slot>
+      </div>
     </header>
-    <hr />
+    <hr class="separator" />
     <div class="posts">
       <Post v-for="post in filteredPosts[currentPage]" :key="post.id" 
         :title="post.title" :body="post.body" />
     </div>
-    <hr />
+    <hr class="separator" />
     <footer>
-      <button @click="currentPage > 0 && currentPage--"
+      <button class="nav-buttons"
+        @click="currentPage > 0 && currentPage--"
         :disabled="currentPage <= 0">
         &LeftArrow;
       </button>
-      <span>Página {{ currentPage + 1 }} de {{ filteredPosts.length }}</span>
-      <button 
+      <span>
+        Página 
+        <strong>{{ currentPage + 1 }} </strong>
+        de
+        <strong>{{ filteredPosts.length }}</strong>
+      </span>
+      <button class="nav-buttons"
         @click="currentPage < filteredPosts.length - 1 && currentPage++"
         :disabled="currentPage >= filteredPosts.length - 1">
         &RightArrow;
@@ -42,5 +50,52 @@ const filteredPosts = filterPosts(posts, filter, true)
 </template>
 
 <style lang="css" scoped>
+  :deep(button) {
+    background: none;
+    padding: .5rem;
+    border: 2px solid white;
+    color: white;
+    cursor: pointer;
+  }
+  :deep(button):enabled:active {
+    background-color: white;
+    color: var(--dark-gray-1);
+  }
+  .nav-buttons {
+    width: 2rem;
+    height: 2rem;
+    line-height: 0px;
+    border-radius: 2rem;
+  }
+  .nav-buttons:disabled {
+    opacity: 0.5;
+  }
+  .separator {
+    margin-block: 1rem;
+    color: white;
+  }
+
+  .feed {
+    header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
   
+      .search-wrapper {
+        display: inherit;
+        gap: .5rem;
+      }
+      input {
+        padding: .5rem;
+        border: 2px solid var(--dark-gray-1);
+        border-radius: .75rem;
+      }
+    }
+    footer {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: .5rem;
+    }
+  } 
 </style>
